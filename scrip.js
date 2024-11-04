b4c53551516c4688a4dcdedfafda57ed78124c21
Diego <creacioneszaide@gmail.com>
2024-11-03 22:08:04 -0500

    ingreso variable de estado

---
+++ scrip.js
@@ -0,0 +1,5 @@
+// ingreso variable que pide nombre al usuario
+let nombre =prompt("ingresa nombre");
+
+//ingreso variable de estado civil
+let estado =prompt("ingresa tu estado civil(1:sotero, 2:casado, 3:separado, 4:viudo, 5:union libre):");

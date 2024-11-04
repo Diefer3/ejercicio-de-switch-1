ea91b4dee454506772f6bd2fb57f5fde58a1690f
Diego <creacioneszaide@gmail.com>
2024-11-03 22:17:43 -0500

    ingreso switch

--- scrip.js
+++ scrip.js
@@ -3,3 +3,23 @@ let nombre =prompt("ingresa nombre");
 
 //ingreso variable de estado civil
 let estado =prompt("ingresa tu estado civil(1:sotero, 2:casado, 3:separado, 4:viudo, 5:union libre):");
+
+// ingreso switch de los casos 
+switch ("estado") {
+    case "1":
+        estadocivil = "soltero";
+        break;
+    case "2":
+        estadocivil = "casado";
+        break;
+    case "3":
+        estadocivil = "separado";
+            break;    
+    case "4":
+        estadocivil = "viudo";
+    break;
+    case "5":
+        estadocivil = "union libre";
+        break;
+
+}
